850509ecd20f61f11eb8d5ccdffba1cab6f9c940
Amartya Upmanyu <upmanyu07amartya@gmail.com>
2026-08-19 20:50:43 +0530

    middlewares

--- src/app.js
+++ src/app.js
@@ -1,18 +1,13 @@
 const express = require("express");
+const { adminAuth } = require("./middlewares/auth");
 
 const app = express();
 
-app.get("/",(req,res)=>{
-    res.send("Hello from Home Page");
-});
-
-app.get("/user",(req,res)=>{
-    res.send({firstName:"Amartya",lastName:"Upmanyu"});
+// Handle Auth Middleware for all requests;
+app.use("/admin", adminAuth);
+app.get("/admin/getAllData",(req,res)=>{
+    res.send("All Data");
 })
-
-app.post("/user",(req,res)=>{
-    res.send("User created");
-});
 
 app.listen(3000,()=>{
     console.log('Server is running on PORT 3000');
---
+++ src/middlewares/auth.js
@@ -0,0 +1,11 @@
+const adminAuth = (req, res, next) => {
+    const token = "xyz";
+    const isAuthorized = token === "xyz";
+    if (!isAuthorized) {
+        res.status(401).send("Unauthorized request")
+    } else {
+        next()
+    }   
+};
+
+module.exports = { adminAuth };
