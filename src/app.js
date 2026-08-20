37dfcedc852671612e01ee1b640b207afec42e2d
Amartya Upmanyu <upmanyu07amartya@gmail.com>
2026-08-20 21:04:01 +0530

    mongoose + DB Connection

--- src/app.js
+++ src/app.js
@@ -1,14 +1,17 @@
 const express = require("express");
-const { adminAuth } = require("./middlewares/auth");
+const {connectDB} = require('./config/database');
 
 const app = express();
 
-// Handle Auth Middleware for all requests;
-app.use("/admin", adminAuth);
-app.get("/admin/getAllData",(req,res)=>{
-    res.send("All Data");
+
+// Connected to DB and only on successful connection we start the server
+connectDB().then(()=>{
+    console.log("Database connected successfully");
+    app.listen(3000, () => {
+      console.log("Server is running on PORT 3000");
+    });
+
+}).catch(err=>{
+    console.error("Database connection failed",err);
 })
 
-app.listen(3000,()=>{
-    console.log('Server is running on PORT 3000');
-})
---
+++ src/config/database.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const connectDB = async()=>{
+    await mongoose.connect(
+      "mongodb+srv://upmanyu07amartya_db_user:8dvNODvBThpGfRYJ@cluster0.n7ipxlh.mongodb.net/",
+    );
+};
+
+module.exports = {connectDB}
+
