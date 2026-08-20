4159dfaa3607444f43ba694a7659bb2e4af62714
Amartya Upmanyu <upmanyu07amartya@gmail.com>
2026-08-20 21:30:22 +0530

    database init

--- src/app.js
+++ src/app.js
@@ -1,17 +1,32 @@
 const express = require("express");
-const {connectDB} = require('./config/database');
+const { connectDB } = require("./config/database");
+const User = require("./models/user");
 
 const app = express();
 
+app.post("/signup", async (req, res) => {
+  const user = new User({
+    firstName: "Amartya",
+    lastName: "Upmanyu",
+    email: "amartyaupmanyu@gmail.com",
+    password: "123456",
+  });
+  try {
+    await user.save();    // create a new user instance and save it to the database
+    res.send("User created Successfully");
+  } catch (err) {
+    res.status(500).send("Error creating user");
+  }
+});
 
 // Connected to DB and only on successful connection we start the server
-connectDB().then(()=>{
+connectDB()
+  .then(() => {
     console.log("Database connected successfully");
     app.listen(3000, () => {
       console.log("Server is running on PORT 3000");
     });
-
-}).catch(err=>{
-    console.error("Database connection failed",err);
-})
-
+  })
+  .catch((err) => {
+    console.error("Database connection failed", err);
+  });
--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async()=>{
     await mongoose.connect(
-      "mongodb+srv://upmanyu07amartya_db_user:8dvNODvBThpGfRYJ@cluster0.n7ipxlh.mongodb.net/",
+      "mongodb+srv://upmanyu07amartya_db_user:8dvNODvBThpGfRYJ@cluster0.n7ipxlh.mongodb.net/devTinder",
     );
 };
 
