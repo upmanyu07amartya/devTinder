b8294e9abed0a7f91021daeb955ed38a5c58ce97
Amartya Upmanyu <upmanyu07amartya@gmail.com>
2026-08-21 11:13:09 +0530

    get APIs

--- src/app.js
+++ src/app.js
@@ -4,20 +4,40 @@ const User = require("./models/user");
 
 const app = express();
 
+app.use(express.json()); // Middleware to parse JSON request bodies
+
 app.post("/signup", async (req, res) => {
-  const user = new User({
-    firstName: "Amartya",
-    lastName: "Upmanyu",
-    email: "amartyaupmanyu@gmail.com",
-    password: "123456",
-  });
+  const user = new User(req.body); // create a new user instance with the request body data
   try {
-    await user.save();    // create a new user instance and save it to the database
+    await user.save();  // save it to the database
     res.send("User created Successfully");
   } catch (err) {
     res.status(500).send("Error creating user");
   }
 });
+
+app.get("/user", async(req,res)=>{
+  const userEmail = req.body.email;
+  try{
+    const user = await User.findOne({ email: userEmail });
+    if(!user){
+      res.status(404).send("User not found");
+    }else{
+      res.send(user);
+    }
+  }catch(err){
+    res.status(500).send("Error Fetching user");
+  }
+})
+
+app.get("/users", async(req,res)=>{
+  try{
+    const users = await User.find() // fetch all users from the database
+    res.send(users); 
+  }catch(err){
+    res.status(500).send("Error fetching users");
+  }
+})
 
 // Connected to DB and only on successful connection we start the server
 connectDB()
