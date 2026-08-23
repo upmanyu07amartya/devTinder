44d8057b3a21ce6fa69a40643b73a8045eb26fd4
Amartya Upmanyu <upmanyu07amartya@gmail.com>
2026-08-23 21:56:36 +0530

    user auth middleware

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,11 +1,19 @@
-const adminAuth = (req, res, next) => {
-    const token = "xyz";
-    const isAuthorized = token === "xyz";
-    if (!isAuthorized) {
-        res.status(401).send("Unauthorized request")
-    } else {
-        next()
-    }   
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+
+const userAuth = async (req, res, next) => {
+  try {
+    const { token } = req.cookies;
+    if (!token) throw new Error("Invalid Token !!!!");
+    const decodedObj = await jwt.verify(token, "DEV@Tinder$790");
+    const { _id } = decodedObj;
+    const user = await User.findById(_id);
+    if (!user) throw new Error("User not found!!!");
+    req.user = user;
+    next();
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
 };
 
-module.exports = { adminAuth };
+module.exports = { userAuth };
