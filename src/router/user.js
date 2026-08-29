3699009d8b9dfbfc1f53b308d5c603e666dfc5fa
Amartya Upmanyu <upmanyu07amartya@gmail.com>
2026-08-29 16:08:37 +0530

    user feed api

--- src/router/user.js
+++ src/router/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const userRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 
 const USER_SAFE_DATA = [
   "firstName",
@@ -45,13 +46,44 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
     if (connections.length === 0)
       res.json({ message: "No Connections Found", data: [] });
     const data = connections.map((row) => {
-      if (row.fromUserId.toString() === loggedInUser._id.toString()) {
+      if (row.fromUserId._id.toString() === loggedInUser._id.toString()) {
         return row.toUserId;
       }
 
       return row.fromUserId;
     });
     res.json({ message: "Connections Found", data });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+userRouter.get("/feed", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+    // get All connection requests involving the loggedIn user
+
+    const connectionRequests = await ConnectionRequest.find({
+      $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+    }).select("fromUserId toUserId");
+
+    // construct a unique set of users that should be hidden
+    const usersToHide = new Set();
+    connectionRequests.forEach((req) => {
+      usersToHide.add(req.fromUserId.toString());
+      usersToHide.add(req.toUserId.toString());
+    });
+
+    //get user info to show.
+
+    const users = await User.find({
+      $and: [
+        { _id: { $nin: Array.from(usersToHide) } },         // this will actually omit the loggedinuser id (which is correct btw)
+        { _id: { $ne: loggedInUser._id } },               // this is in case if loggedinuser has never sent or received a connection request
+      ],
+    }).select(USER_SAFE_DATA);
+
+    res.json({ data: users });
   } catch (err) {
     res.status(400).json({ message: err.message });
   }
